8082e279853d04999edd4f5a4649c62db5ca4f31
JuhanaTa <61382156+JuhanaTa@users.noreply.github.com>
2020-06-11 14:32:06 +0300

    git fixes in task

---
+++ osa2/maidentiedot/src/App.js
@@ -0,0 +1,70 @@
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import Country from './components/Country'
+import OneCountry from './components/OneCountry'
+
+function App() {
+  const [countries, setCountries] = useState([])
+  const [countryFilter, setCountryFilter] = useState('')
+  const [country, setCountry] = useState([])
+  //boolean used for hiding and making visible search results
+  const [filterBool, setBool] = useState(true)
+  let alert = ""
+
+  const hook = () => {
+    console.log('effect')
+    axios
+      .get('https://restcountries.eu/rest/v2/all')
+      .then(response => {
+        console.log('promise fulfilled')
+        setCountries(response.data)
+      })
+  }
+  useEffect(hook, [])
+
+  const handleSearch = (event) =>{
+    //every change in search bar sets boolean false 
+    setBool(false)
+    console.log(filterBool, "bool changed")
+    setCountryFilter(event.target.value.toLowerCase())
+    setCountry([])
+  }
+
+
+  let countriesToShow = []
+  
+  if(countryFilter.length > 0){
+    countriesToShow = countries.filter(country => country.name.toLowerCase().includes(countryFilter))
+    console.log(countriesToShow)
+  }
+  if(countriesToShow.length > 10 ){
+    countriesToShow = []
+    alert = "too many matches"
+  }
+ //additional info shown if length of array is 1 and filter boolean is false
+  if(countriesToShow.length === 1 && filterBool === false){
+    console.log("one country detected")
+    setBool(true)
+    setCountry(countriesToShow)
+    console.log(countriesToShow)
+    alert = []
+  }
+  
+  return (
+    <div>
+      find countries: <input onChange={handleSearch}/>
+      {countriesToShow.map((country, i) =>
+      <Country key={i} country={country}/>
+      )}
+      <div>{alert}</div>
+      <div>
+        {country.map((one, i) =>
+        <OneCountry key={one.name} one={one}/>
+        )}
+      </div>
+    </div>
+  );
+  
+}
+
+export default App;
---
+++ osa2/maidentiedot/src/components/OneCountry.js
@@ -0,0 +1,19 @@
+import React from 'react'
+const OneCountry = ({one}) => {
+    return (
+        <div>
+            
+            <h1>{one.name}</h1>
+            <p>{one.capital}</p>
+            <p>{one.population}</p>
+            <h2>languages</h2>
+            <ul>
+            {one.languages.map((language, i) =>
+            <li key={i}>{language.name}</li>)}
+            </ul>
+            <img src={one.flag} alt="lippu" width="20%" ></img>
+            
+        </div>
+    )
+}
+export default OneCountry
